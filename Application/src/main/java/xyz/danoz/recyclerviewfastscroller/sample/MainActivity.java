25d4e07d1451d24538a639d1d3530c518accc951
Christoph Kaser <berlin@caser.de>
2023-08-29 12:25:02 +0200

    upgrade gradle, buildtools etc

--- Application/src/main/java/xyz/danoz/recyclerviewfastscroller/sample/MainActivity.java
+++ Application/src/main/java/xyz/danoz/recyclerviewfastscroller/sample/MainActivity.java
@@ -1,18 +1,18 @@
 /*
-* Copyright 2013 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2013 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package xyz.danoz.recyclerviewfastscroller.sample;
 
@@ -36,50 +36,49 @@ import xyz.danoz.recyclerviewfastscroller.vertical.VerticalRecyclerViewFastScrol
  */
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main);
 
-        if (savedInstanceState == null) {
-            changeSection(RecyclerViewWithFastScrollerFragment.class);
-        }
-    }
+		if (savedInstanceState == null) {
+			changeSection(RecyclerViewWithFastScrollerFragment.class);
+		}
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_options_menu, menu);
-        return true;
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_options_menu, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_item_list_no_section_indicator:
-                changeSection(RecyclerViewWithFastScrollerFragment.class);
-                return true;
-            case R.id.menu_item_list_with_sections:
-                changeSection(RecyclerViewWithSectionIndicatorFragment.class);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int itemId = item.getItemId();
+		if (itemId == R.id.menu_item_list_no_section_indicator) {
+			changeSection(RecyclerViewWithFastScrollerFragment.class);
+			return true;
+		} else if (itemId == R.id.menu_item_list_with_sections) {
+			changeSection(RecyclerViewWithSectionIndicatorFragment.class);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
-    private void changeSection(Class<? extends Fragment> fragment) {
-        try {
-            Fragment sectionsFragment = fragment.newInstance();
-            replaceCurrentFragment(sectionsFragment);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	private void changeSection(Class<? extends Fragment> fragment) {
+		try {
+			Fragment sectionsFragment = fragment.newInstance();
+			replaceCurrentFragment(sectionsFragment);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    private void replaceCurrentFragment(Fragment newFragment) {
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.sample_content_fragment, newFragment);
-        transaction.commit();
-    }
+	private void replaceCurrentFragment(Fragment newFragment) {
+		FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+		transaction.replace(R.id.sample_content_fragment, newFragment);
+		transaction.commit();
+	}
 }
